b6759fb4d032e8b0ae6ad3acfb37077fadc3dbd8
shektor <s.hektor@gmail.com>
2020-04-13 13:23:35 +0100

    Import package from module

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/shektor/hello-go/morestrings"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
