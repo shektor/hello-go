016bcb0814482b540fd8cc77d083851537863229
shektor <s.hektor@gmail.com>
2020-04-13 16:39:47 +0100

    Import package from remote module

--- hello.go
+++ hello.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"github.com/shektor/hello-go/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
